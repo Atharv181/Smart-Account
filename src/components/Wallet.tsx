c8e1a32ab5f92e0e221a230fb4f07794c3849f83
Atharv181 <atharvbobade181@gmail.com>
2023-10-07 17:01:53 +0530

    Login function added

--- src/components/Wallet.tsx
+++ src/components/Wallet.tsx
@@ -1,5 +1,7 @@
 import { useEffect, useRef, useState } from "react";
 import SocialLogin from "@biconomy/web3-auth";
+import { ethers } from "ethers";
+import { ChainId } from "@biconomy/core-types";
 
 export default function Wallet(){
     const sdkRef = useRef<SocialLogin | null>(null);
@@ -15,7 +17,26 @@ export default function Wallet(){
             }, 1000);
         }
     }, [interval]);
+
+    async function login(){
+        if(!sdkRef.current){
+            const socialLoginSDK = new SocialLogin();
+            await socialLoginSDK.init({
+                chainId: ethers.utils.hexValue(ChainId.POLYGON_MUMBAI).toString(),
+                network: "testnet",
+            });
+            sdkRef.current = socialLoginSDK;
+        }
+
+        if(!sdkRef.current.provider){
+            sdkRef.current.showWallet();
+            enableInterval(true);
+        } else {
+            console.log("hello");
+        }
+    }
 }
+
 
 
 
