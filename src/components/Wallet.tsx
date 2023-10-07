24f8a4dd3afb81dc21203c4dfbfd233ad51c3fa6
Atharv181 <atharvbobade181@gmail.com>
2023-10-07 20:27:17 +0530

    Wallet setup added

--- src/components/Wallet.tsx
+++ src/components/Wallet.tsx
@@ -1,11 +1,15 @@
 import { useEffect, useRef, useState } from "react";
 import SocialLogin from "@biconomy/web3-auth";
-import { ethers } from "ethers";
+import { ethers, providers } from "ethers";
 import { ChainId } from "@biconomy/core-types";
+import { BiconomySmartAccount, BiconomySmartAccountConfig } from "@biconomy/account";
 
 export default function Wallet(){
     const sdkRef = useRef<SocialLogin | null>(null);
     const [interval, enableInterval] = useState<boolean>(false);
+    const [loading, setLoading] = useState(false);
+    const [, setProvider] = useState<providers.Web3Provider>();
+    const [smartAccount, setSmartAccount] = useState<BiconomySmartAccount>();
 
     useEffect(() =>{
         let configureLogin: NodeJS.Timeout | undefined;
@@ -34,6 +38,38 @@ export default function Wallet(){
         } else {
             console.log("hello");
         }
+    }
+
+    async function steupSmartAccount() {
+        try {
+            if (!sdkRef.current?.provider) {
+                return; 
+            }
+            //It will hide the wallet if it is already open
+            sdkRef.current.hideWallet();
+
+            setLoading(true);
+
+            let web3Provider = new ethers.providers.Web3Provider(
+                sdkRef.current.provider
+            );
+            setProvider(web3Provider);
+            const config : BiconomySmartAccountConfig = {
+                signer : web3Provider.getSigner(),
+                chainId : ChainId.POLYGON_MUMBAI,
+                bundler : bundler,
+                paymaster : paymaster,
+            };
+
+            const smartAccount = new BiconomySmartAccount(config);
+            await smartAccount.init();
+
+            setSmartAccount(smartAccount);
+        } catch (e) {
+            console.error(e);
+        }
+
+        setLoading(false);
     }
 }
 
